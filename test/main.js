4acd97e8cbdaac6e410018144a53e508ae8ccc78
Andreas Lind <andreaslindpetersen@gmail.com>
2019-07-23 00:29:43 +0200

    Test --no-fallback mode

--- test/main.js
+++ test/main.js
@@ -16,7 +16,7 @@ const openSansBold = require('fs').readFileSync(
 describe('subfont', function() {
   let mockConsole;
   beforeEach(async function() {
-    mockConsole = { log: sinon.spy() };
+    mockConsole = { log: sinon.spy(), error: console.error };
   });
 
   describe('when a font is referenced by a stylesheet hosted outside the root', function() {
@@ -130,6 +130,70 @@ describe('subfont', function() {
 
       const cssAsset = assetGraph.findAssets({ fileName: 'styles.css' })[0];
       expect(cssAsset.url, 'to equal', 'https://mycdn.com/styles.css');
+    });
+  });
+
+  describe('with --no-fallbacks', function() {
+    it('should leave out the fallbacks', async function() {
+      httpception([
+        {
+          request: 'GET https://example.com/',
+          response: {
+            headers: {
+              'Content-Type': 'text/html'
+            },
+            body: `
+              <!DOCTYPE html>
+              <html>
+
+              <head>
+                <style>
+                  @font-face {
+                    font-family: Open Sans;
+                    font-style: normal;
+                    font-weight: 700;
+                    src: url(http://themes.googleusercontent.com/static/fonts/opensans/v8/k3k702ZOKiLJc3WVjuplzHhCUOGz7vYGh680lGh-uXM.woff) format('woff');
+                  }
+
+                  div {
+                    font-family: Open Sans;
+                  }
+                </style>
+              </head>
+              <body>
+                <div>Hello</div>
+              </body>
+              </html>
+            `
+          }
+        },
+        {
+          request:
+            'GET http://themes.googleusercontent.com/static/fonts/opensans/v8/k3k702ZOKiLJc3WVjuplzHhCUOGz7vYGh680lGh-uXM.woff',
+          response: {
+            headers: {
+              'Content-Type': 'font/woff'
+            },
+            body: openSansBold
+          }
+        }
+      ]);
+
+      const rootUrl = 'https://example.com/';
+      const assetGraph = await main(
+        ['--silent', '--dryrun', '--no-fallbacks', '--root', rootUrl, rootUrl],
+        mockConsole
+      );
+
+      const inlineCssAsset = assetGraph.findAssets({
+        type: 'Css',
+        isInline: true
+      })[0];
+      expect(
+        inlineCssAsset.text,
+        'to contain',
+        "font-family: 'Open Sans__subset';"
+      );
     });
   });
 
