0898bcf1b60232fc58dd27fa07b86d56cd03f19a
Andreas Lind <andreaslindpetersen@gmail.com>
2019-07-29 00:16:32 +0200

    More (failing) subsetLocalFontWithHarfbuzz tests

--- test/subsetLocalFontWithHarfbuzz.js
+++ test/subsetLocalFontWithHarfbuzz.js
@@ -133,4 +133,69 @@ describe('subsetLocalFontWithHarfbuzz', function() {
       expect(result.slice(0, 4).toString(), 'to equal', 'wOF2');
     });
   });
+
+  describe('with a woff2 font', function() {
+    it('should produce a subset as ttf', async function() {
+      const openSansWoff = await readFileAsync(
+        pathModule.resolve(
+          __dirname,
+          '..',
+          'testdata',
+          'subsetFonts',
+          'Roboto-400.woff2'
+        )
+      );
+      const result = await subsetLocalFontWithHarfbuzz(
+        openSansWoff,
+        'ttf',
+        'abcd'
+      );
+
+      expect(result, 'to be a', 'Buffer');
+      expect(result.length, 'to be less than', openSansWoff.length);
+      // TODO: Check for truetype signature/magic?
+    });
+
+    it('should produce a subset as woff', async function() {
+      const openSansWoff = await readFileAsync(
+        pathModule.resolve(
+          __dirname,
+          '..',
+          'testdata',
+          'subsetFonts',
+          'Roboto-400.woff2'
+        )
+      );
+      const result = await subsetLocalFontWithHarfbuzz(
+        openSansWoff,
+        'woff',
+        'abcd'
+      );
+
+      expect(result, 'to be a', 'Buffer');
+      expect(result.length, 'to be less than', openSansWoff.length);
+      expect(result.slice(0, 4).toString(), 'to equal', 'wOFF');
+    });
+
+    it('should produce a subset as woff2', async function() {
+      const openSansWoff = await readFileAsync(
+        pathModule.resolve(
+          __dirname,
+          '..',
+          'testdata',
+          'subsetFonts',
+          'Roboto-400.woff2'
+        )
+      );
+      const result = await subsetLocalFontWithHarfbuzz(
+        openSansWoff,
+        'woff2',
+        'abcd'
+      );
+
+      expect(result, 'to be a', 'Buffer');
+      expect(result.length, 'to be less than', openSansWoff.length);
+      expect(result.slice(0, 4).toString(), 'to equal', 'wOF2');
+    });
+  });
 });
