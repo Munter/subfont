636a4a6fef2cd27077874c8d4a2f4d18b1b08176
Andreas Lind <andreaslindpetersen@gmail.com>
2019-10-23 00:02:31 +0200

    Remove unused function, whoops

--- test/cli.js
+++ test/cli.js
@@ -1,7 +1,6 @@
 const pathModule = require('path');
-const expect = require('unexpected').clone();
-
 const childProcess = require('child_process');
+const expect = require('unexpected').clone();
 
 function consumeStream(stream) {
   return new Promise((resolve, reject) => {
@@ -11,48 +10,6 @@ function consumeStream(stream) {
       .on('end', () => resolve(Buffer.concat(buffers)))
       .on('error', reject);
   });
-}
-
-async function run(commandAndArgs, stdin) {
-  if (typeof commandAndArgs !== 'undefined' && !Array.isArray(commandAndArgs)) {
-    commandAndArgs = [commandAndArgs];
-  }
-
-  const proc = childProcess.spawn(commandAndArgs[0], commandAndArgs.slice(1));
-
-  const promises = {
-    exit: new Promise((resolve, reject) => {
-      proc.on('error', reject).on('exit', exitCode => {
-        if (exitCode === 0) {
-          resolve();
-        } else {
-          const err = new Error(`Child process exited with ${exitCode}`);
-          err.exitCode = exitCode;
-          reject(err);
-        }
-      });
-    }),
-    stdin: new Promise((resolve, reject) => {
-      proc.stdin.on('error', reject).on('close', resolve);
-    }),
-    stdout: consumeStream(proc.stdout),
-    stderr: consumeStream(proc.stderr)
-  };
-
-  if (typeof stdin === 'undefined') {
-    proc.stdin.end();
-  } else {
-    proc.stdin.end(stdin);
-  }
-
-  try {
-    await Promise.all(Object.values(promises));
-    return [await promises.stdout, await promises.stderr];
-  } catch (err) {
-    err.stdout = await promises.stdout;
-    err.stderr = await promises.stderr;
-    throw err;
-  }
 }
 
 async function runSubfont(...args) {
