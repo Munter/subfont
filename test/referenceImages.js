ee88c9f91973a7dc0a0cb6899b562b34d1e5edbf
Andreas Lind <andreaslindpetersen@gmail.com>
2020-07-17 09:51:27 +0200

    Pull in refactoring of the reference images tests from feature/harfbuzz

--- test/referenceImages.js
+++ test/referenceImages.js
@@ -1,69 +1,47 @@
 const expect = require('./expect');
+const combos = require('combos');
 
 describe('reference images', function () {
-  for (const inlineCss of [true, false]) {
-    describe(`with inlineCss:${inlineCss}`, function () {
-      for (const inlineFonts of [true, false]) {
-        describe(`with inlineFonts:${inlineFonts}`, function () {
-          for (const omitFallbacks of [true, false]) {
-            describe(`with omitFallbacks:${omitFallbacks}`, function () {
-              it('should render a simple test case without ligatures', async function () {
-                await expect(
-                  'withoutLigatures',
-                  'to render the same after subsetting',
-                  {
-                    inlineCss,
-                    inlineFonts,
-                    omitFallbacks,
-                  }
-                );
-              });
+  for (const options of combos({
+    inlineCss: [false, true],
+    inlineSubsets: [false, true],
+    omitFallbacks: [false, true],
+    dynamic: [false, true],
+  })) {
+    describe(`with ${Object.keys(options)
+      .map((key) => `${key}: ${options[key]}`)
+      .join(', ')}`, function () {
+      it('should render a simple test case without ligatures', async function () {
+        await expect(
+          'withoutLigatures',
+          'to render the same after subsetting',
+          options
+        );
+      });
 
-              it('should render ligatures correctly', async function () {
-                await expect(
-                  'ligatures',
-                  'to render the same after subsetting',
-                  {
-                    inlineCss,
-                    inlineFonts,
-                    omitFallbacks,
-                  }
-                );
-              });
+      it('should render ligatures correctly', async function () {
+        await expect(
+          'ligatures',
+          'to render the same after subsetting',
+          options
+        );
+      });
 
-              for (const dynamic of [true, false]) {
-                describe(`with dynamic:${dynamic}`, function () {
-                  it('should render missing glyphs', async function () {
-                    await expect(
-                      'missingGlyphs',
-                      'to render the same after subsetting',
-                      {
-                        inlineCss,
-                        inlineFonts,
-                        omitFallbacks,
-                        dynamic,
-                      }
-                    );
-                  });
+      it('should render missing glyphs', async function () {
+        await expect(
+          'missingGlyphs',
+          'to render the same after subsetting',
+          options
+        );
+      });
 
-                  it('should render unused variants', async function () {
-                    await expect(
-                      'unusedVariants',
-                      'to render the same after subsetting',
-                      {
-                        inlineCss,
-                        inlineFonts,
-                        omitFallbacks,
-                        dynamic,
-                      }
-                    );
-                  });
-                });
-              }
-            });
-          }
-        });
-      }
+      it('should render unused variants', async function () {
+        await expect(
+          'unusedVariants',
+          'to render the same after subsetting',
+          options
+        );
+      });
     });
   }
 });
