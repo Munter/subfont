8db6e627322a9cf0473ac097e9571dba692aef5e
Andreas Lind <andreaslindpetersen@gmail.com>
2022-08-22 23:15:00 +0200

    Implement a more tolerant font-variant-settings parser

---
+++ test/parseFontVariationSettings.js
@@ -0,0 +1,50 @@
+const parseFontVariationSettings = require('../lib/parseFontVariationSettings');
+
+describe('parseFontVariationSettings', function () {
+  const expect = require('unexpected')
+    .clone()
+    .addAssertion(
+      '<string> to come out as <array>',
+      (expect, subject, expectedValue) => {
+        const parsedValue = [...parseFontVariationSettings(subject)];
+        expect(parsedValue, 'to equal', expectedValue);
+      }
+    );
+
+  it('should uppercase axis names', function () {
+    expect('"foob" 200, "quux" 400', 'to come out as', [
+      ['FOOB', 200],
+      ['QUUX', 400],
+    ]);
+  });
+
+  it('should ignore extra whitespace', function () {
+    expect(' "FOOB" 200 , "QUUX" 400', 'to come out as', [
+      ['FOOB', 200],
+      ['QUUX', 400],
+    ]);
+  });
+
+  describe('with unsupported input', function () {
+    it('should ignore content after double comma', function () {
+      expect('"FOOB" 200,, "QUUX" 400', 'to come out as', [['FOOB', 200]]);
+    });
+
+    it('should ignore content after an axis name given as a non-string', function () {
+      expect('"FOOB" 200, 123 400, "BAAZ" 800', 'to come out as', [
+        ['FOOB', 200],
+      ]);
+    });
+
+    it('should ignore a value given as custom property', function () {
+      expect('"FOOB" 200, "QUUX" var(--blah), "BAAZ" 800', 'to come out as', [
+        ['FOOB', 200],
+        ['BAAZ', 800],
+      ]);
+    });
+
+    it('should ignore a value given as a string', function () {
+      expect('"FOOB" "200", "BAAZ" 800', 'to come out as', [['BAAZ', 800]]);
+    });
+  });
+});
